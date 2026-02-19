30e58ee9931193545edca862b1a34d2ccb65cd81
Noah Sherman <146763946+nsherman24@users.noreply.github.com>
2026-02-19 12:31:32 -0700

    fixed migrations issue and registered rooms

--- scheduler/scheduler/core/admin.py
+++ scheduler/scheduler/core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import AvailabilityRange, Team, Role, Shift
+from .models import AvailabilityRange, Team, Role, Shift, Room, RoomAvailability
 
 admin.site.register(AvailabilityRange)
 admin.site.register(Team)
 admin.site.register(Role)
 admin.site.register(Shift)
+admin.site.register(Room)
+admin.site.register(RoomAvailability)
